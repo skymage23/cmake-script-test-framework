3912f82b1e5b2c6bda055650809f33e8b92a3c49
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-01-28 13:29:54 -0600

    Added SKIP_GENERATE_FILE as an argument to "run_test" to allow skipping regenerating the test file and just running it.

--- cmake-test-runner.cmake
+++ cmake-test-runner.cmake
@@ -1,30 +1,56 @@
-find_package(Python3)
-
+find_package(Python)
 
 set(TEMP "${CMAKE_CURRENT_LIST_DIR}")
 file(REAL_PATH "${TEMP}" CURRENT_SCRIPT_DIR)
 
-file(DIRECTORY "${TEMP}" TEMP)
+set(PYTHON_SCRIPT_DIR_PATH "${TEMP}/python")
+set(PYTHON_TEST_GENERATOR_SCRIPT_PATH "${PYTHON_SCRIPT_DIR_PATH}/generate-test-file.py")
+set(GENERATED_TEST_DIR_PATH "${PYTHON_SCRIPT_DIR_PATH}/tests")
 
-set(PYTHON_TEST_GENERATOR_SCRIPT_PATH "")
-set(TEMP "${PYTHON_TEST_GENERATOR_SCRIPT_PATH}/python/generate-test-file.py")
 
+
+set(TEST_SUCCESS FALSE)
+
+#
+# SKIP_GENERATE_FILE is ignored if the generated test file does not exist.
+#
 function(run_test)
-   set(TEST_SCRIPT_FILENAME "${CMAKE_CURRENT_LIST_FILE}")
-   file(NAME ${TEST_SCRIPT_FILENAME} TEST_SCRIPT_FILENAME)
+    if(${ARGC} LESS 2)
+        message(SEND_ERROR "\"run_test\": Too few arguments.")
+        return()
+    endif()
 
-    #generate test file
-    execute_process(
-	COMMAND "${Python::Interpreter}" ${CMAKE_CURRENT_LIST_FILE}
-	WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
-	COMMAND_ERROR_IS_FATAL ANY
-    )
+    if(${ARGC} GREATER 3)
+        message(SEND_ERROR "\"run_test\": Too many arguments.")
+        return()
+    endif()
+
+    set(options "SKIP_GENERATE_FILE")
+    set(oneValueArgs "TEST_SCRIPT_FILE")
+    cmake_parse_arguments(run_test ${options} ${oneValueArgs} "" ${ARGN})
+
+    get_filename_component(TEST_SCRIPT_FILENAME "${run_test_TEST_SCRIPT_FILE}" NAME)
+
+    set(TEST_FILE "${GENERATED_TEST_DIR_PATH}/${TEST_SCRIPT_FILENAME}")
+    
+    if((NOT run_test_SKIP_GENERATE_FILE) OR (NOT EXISTS "${TEST_FILE}"))
+        #generate test file
+        message(STATUS "Generating test file for \"${run_test_TEST_SCRIPT_FILE}\".")
+        execute_process(
+            COMMAND "${Python_EXECUTABLE}" "${PYTHON_TEST_GENERATOR_SCRIPT_PATH}" "${run_test_TEST_SCRIPT_FILE}"
+            WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+    endif()
 
     #run generated test file
+    message(STATUS "Executing test file for \"${run_test_TEST_SCRIPT_FILE}\".")
     execute_process(
-        COMMAND "${CMAKE_COMMAND}" -P "${TEST_SCRIPT_FILENAME}"
-	WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
-	COMMAND_ERROR_IS_FATAL ANY
+        COMMAND "${CMAKE_COMMAND}" -P "${TEST_FILE}"
+	    WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+	    COMMAND_ERROR_IS_FATAL ANY
     )
 
+    #If we get to this point, the tests have passed.
+    set(TEST_SUCCESS TRUE PARENT_SCOPE)
 endfunction()
