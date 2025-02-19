8bb350211d113710a800397ad1b526c47e580c81
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-02-19 11:47:55 -0800

    Added more tests to 'filepath_helper_tests.py' to try to account for previously problematice edge cases: dot references.  Also added tests to account for spaces in directory names/filenames, and for trying to parse UNC file paths on a UNIX system.

--- tests/test_runners/test_runner_lib/filepath_helper_tests.py
+++ tests/test_runners/test_runner_lib/filepath_helper_tests.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-import unittest
-import common
-
 import importlib
 import sys
-sys.path.append(common.scripts_dir.__str__())
+import os
+import unittest
 
+
+import common
+
+sys.path.append(common.scripts_dir.__str__())
 fp_helper = importlib.import_module("filepath_helper")
 
 class TestFilepathHelper(unittest.TestCase):
@@ -38,61 +40,110 @@ class TestFilepathHelper(unittest.TestCase):
         expected_output = ['', 'path', 'to', ' sample ', 'file']
         self.assertEqual(output, expected_output)
 
+    #
+    # The expected behavior is that when we are not running
+    # on a system that uses UNC pathing (meaning, anything
+    # other than Windows, DOS, or ReactOS), the entire
+    # proposed path string is parsed as if it was a 
+    # directory name/file name altogether. Thus,
+    # when we attempt to split it, the result should
+    # be a single-element array whose only element is
+    # identical to the split function input, including
+    # the drive letter.
+    #
     def test_filepath_helper_unc(self):
         sample_path=R'C:\Users\Admin\Documents\TestFiles'
         drive_letter, output = fp_helper.split_filepath(sample_path)
-        self.assertEqual (drive_letter, 'C:')
-        expected_output = ['', 'Users', 'Admin', 'Documents', 'TestFiles']
+
+        if(os.name == 'nt'):
+            expected_output = ['', 'Users', 'Admin', 'Documents', 'TestFiles'] 
+            self.assertEqual (drive_letter, 'C:')
+        else:
+            expected_output=[sample_path]
+            self.assertIsNone(drive_letter)
         self.assertEqual(output, expected_output)
+        
 
     def test_filepath_helper_self_references_unc(self):
         sample_path=R'C:\Users\Admin\Documents\.\TestFiles'
         drive_letter, output = fp_helper.split_filepath(sample_path)
-        self.assertEqual (drive_letter, 'C:')
-        expected_output = ['', 'Users', 'Admin', 'Documents', '.', 'TestFiles']
+        
+        if(os.name == 'nt'):
+            expected_output = ['', 'Users', 'Admin', 'Documents', '.', 'TestFiles']
+            self.assertEqual (drive_letter, 'C:')
+        else:
+            expected_output=[sample_path]
+            self.assertIsNone(drive_letter)
         self.assertEqual(output, expected_output)
 
     def test_filepath_helper_backreferences_unc(self):
         sample_path=R'C:\Users\Admin\Documents\..\Documents\TestFiles'
         drive_letter, output = fp_helper.split_filepath(sample_path)
-        self.assertEqual (drive_letter, 'C:')
-        expected_output = ['', 'Users', 'Admin', 'Documents', '..', 'Documents', 'TestFiles']
+        
+        if(os.name == 'nt'):
+            expected_output = ['', 'Users', 'Admin', 'Documents', '..', 'Documents', 'TestFiles']
+            self.assertEqual (drive_letter, 'C:')
+        else:
+            expected_output = [sample_path]
+            self.assertIsNone(drive_letter)
         self.assertEqual(output, expected_output)
 
     def test_filepath_helper_spaces_in_dir_name_unc(self):
         sample_path=R'C:\Users\Admin\Documents\ tester \TestFiles'
         drive_letter, output = fp_helper.split_filepath(sample_path)
-        self.assertEqual (drive_letter, 'C:')
-        expected_output = ['', 'Users', 'Admin', 'Documents', ' tester ', 'TestFiles']
+        
+        if(os.name == 'nt'):
+            expected_output = ['', 'Users', 'Admin', 'Documents', ' tester ', 'TestFiles']
+            self.assertEqual (drive_letter, 'C:')
+        else:
+            expected_output = [sample_path]
+            self.assertIsNone(drive_letter)
         self.assertEqual(output, expected_output)
 
 
     def test_filepath_helper_no_drive_letter_unc(self):
         sample_path=R'\Users\Admin\Documents\TestFiles'
         drive_letter, output = fp_helper.split_filepath(sample_path)
+
+        if(os.name == 'nt'): 
+            expected_output = ['', 'Users', 'Admin', 'Documents', 'TestFiles']
+            
+        else:
+            expected_output = [sample_path]
         self.assertIsNone(drive_letter)
-        expected_output = ['', 'Users', 'Admin', 'Documents', 'TestFiles']
         self.assertEqual(output, expected_output)
 
     def test_filepath_helper_self_references_no_drive_letter_unc(self):
         sample_path=R'\Users\Admin\Documents\.\TestFiles'
         drive_letter, output = fp_helper.split_filepath(sample_path)
+
+        if(os.name == 'nt'): 
+            expected_output = ['', 'Users', 'Admin', 'Documents', '.', 'TestFiles']
+        else:
+            expected_output = [sample_path]
         self.assertIsNone(drive_letter)
-        expected_output = ['', 'Users', 'Admin', 'Documents', '.', 'TestFiles']
         self.assertEqual(output, expected_output)
 
     def test_filepath_helper_backreferences_no_drive_letter_unc(self):
         sample_path=R'\Users\Admin\Documents\..\Documents\TestFiles'
         drive_letter, output = fp_helper.split_filepath(sample_path)
+
+        if(os.name == 'nt'): 
+            expected_output = ['', 'Users', 'Admin', 'Documents', '..', 'Documents', 'TestFiles']
+        else:
+            expected_output = [sample_path]
         self.assertIsNone(drive_letter)
-        expected_output = ['', 'Users', 'Admin', 'Documents', '..', 'Documents', 'TestFiles']
         self.assertEqual(output, expected_output)
 
     def test_filepath_helper_spaces_in_dir_name_no_drive_letter_unc(self):
         sample_path=R'\Users\Admin\Documents\ tester \TestFiles'
         drive_letter, output = fp_helper.split_filepath(sample_path)
+
+        if(os.name == 'nt'): 
+            expected_output = ['', 'Users', 'Admin', 'Documents', ' tester ', 'TestFiles']
+        else:
+            expected_output = [sample_path]
         self.assertIsNone(drive_letter)
-        expected_output = ['', 'Users', 'Admin', 'Documents', ' tester ', 'TestFiles']
         self.assertEqual(output, expected_output)
 
 if __name__ == "__main__":
